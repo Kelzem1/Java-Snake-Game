a94fc1366aee22fabdfafab67916707a63a8965b
Rodolfo Rolon <kelzemrod@gmail.com>
2024-10-16 19:08:22 +0200

    Snake head, snake food

--- src/App.java
+++ src/App.java
@@ -1,6 +1,8 @@
+
 import javax.swing.*;
 
 public class App {
+
     public static void main(String[] args) throws Exception {
         int boardWidth = 600;
         int boeardHeight = boardWidth;
@@ -14,6 +16,7 @@ public class App {
 
         SnakeGame snakeGame = new SnakeGame(boardWidth, boeardHeight);
         frame.add(snakeGame);
+        //El titulo ocupa parte de los 600px. Por tanto con el .pack se ajusta sin contar esa barra.
         frame.pack();
     }
 }
--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -2,8 +2,23 @@ import java.awt.*;
 import javax.swing.*;
 
 public class SnakeGame extends JPanel {
+    private class Tile{
+        int x;
+        int y;
+        Tile(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
     int boardWidth;
     int boardHeight;
+    int tileSize = 25;
+
+    //Snake
+    Tile snakeHead;
+
+    //Food
+    Tile food;
 
     public SnakeGame(int boardWidth, int boardHeight) {
         this.boardWidth = boardWidth;
@@ -11,6 +26,22 @@ public class SnakeGame extends JPanel {
 
         setPreferredSize(new Dimension(boardWidth, boardHeight));
         setBackground(Color.black);
+
+        snakeHead = new Tile(5, 5);
+        food = new Tile(10, 10);
+    }
+    public void paintComponent(Graphics g ){
+        super.paintComponent(g);
+        draw(g);
+    }
+    public void draw(Graphics g){
+        //Snake:
+        g.setColor(Color.green);
+        g.fillRect(snakeHead.x * tileSize, snakeHead.y * tileSize, tileSize, tileSize);
+
+        //Food:
+        g.setColor(Color.red);
+        g.fillRect(food.x * tileSize, food.y * tileSize, tileSize, tileSize);
     }
 
     
